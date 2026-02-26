135d3cf43421e056f3a686be19c16064ad89c0ac
Vellasta <yong_sheng_kow@kelaberetiv.sg>
2026-02-26 22:58:47 +0800

    Try fix deploy issue

--- ui/core/proto_utils/action_id.ts
+++ ui/core/proto_utils/action_id.ts
@@ -520,7 +520,7 @@ export class ActionId {
 	private static makeIconUrl(iconLabel: string): string {
 		// return `https://wow.zamimg.com/images/wow/icons/large/${iconLabel}.jpg`;
 
-		return `https://database.turtlecraft.gg/images/icons/large/${iconLabel}.png`
+		return `https://database.turtlecraft.gg/images/icons/large/${iconLabel}.png`;
 	}
 
 	static async getTooltipData(actionId: ActionId): Promise<IconData> {
