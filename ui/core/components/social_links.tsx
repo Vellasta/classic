3fac27f70fcdc57bfe6a6874b4c6e5e56f41b4e3
Vellasta <yong_sheng_kow@kelaberetiv.sg>
2026-03-12 12:13:09 +0800

    Update social links

--- ui/core/components/social_links.tsx
+++ ui/core/components/social_links.tsx
@@ -5,7 +5,7 @@ import { Component } from './component';
 export class SocialLinks extends Component {
 	static buildDiscordLink(): Element {
 		const anchor = (
-			<a href="https://discord.gg/p3DgvmnDCS" target="_blank" className="discord-link link-alt" dataset={{ tippyContent: 'Join us on Discord' }}>
+			<a href="https://discord.gg/esRFYZRyUm" target="_blank" className="discord-link link-alt" dataset={{ tippyContent: 'Join us on Discord' }}>
 				<i className="fab fa-discord fa-lg" />
 			</a>
 		);
@@ -15,7 +15,7 @@ export class SocialLinks extends Component {
 
 	static buildGitHubLink(): Element {
 		const anchor = (
-			<a href="https://github.com/wowsims/classic" target="_blank" className="github-link link-alt" dataset={{ tippyContent: 'Contribute on GitHub' }}>
+			<a href="https://github.com/Vellasta/classic" target="_blank" className="github-link link-alt" dataset={{ tippyContent: 'Contribute on GitHub' }}>
 				<i className="fab fa-github fa-lg" />
 			</a>
 		);
@@ -25,7 +25,7 @@ export class SocialLinks extends Component {
 
 	static buildPatreonLink(): Element {
 		const anchor = (
-			<a href="https://patreon.com/wowsims" target="_blank" className="patreon-link link-alt" dataset={{ tippyContent: 'Support us on Patreon' }}>
+			<a href="https://discord.gg/esRFYZRyUm" target="_blank" className="patreon-link link-alt" dataset={{ tippyContent: 'Support us on Patreon' }}>
 				<i className="fab fa-patreon fa-lg" /> Patreon
 			</a>
 		);
